827f0fe3782527e11bccc66a447c3dbc8806d76e
LAPTOP-CJMNHK4L\Jhorfana <jbonilla@valorca.com>
2019-08-24 15:32:09 -0500

    se guardado direcconamiento

--- mippes/src/main/java/co/com/mippes/services/implementation/DireccionamientoLogicaImpl.java
+++ mippes/src/main/java/co/com/mippes/services/implementation/DireccionamientoLogicaImpl.java
@@ -1,5 +1,39 @@
 package co.com.mippes.services.implementation;
 
-public class DireccionamientoLogicaImpl {
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.com.mippes.dao.DireccionamientoDAO;
+import co.com.mippes.dto.DireccionamientoDTO;
+import co.com.mippes.entity.Direccionamiento;
+import co.com.mippes.services.ConsultaMipres;
+import co.com.mippes.services.DireccionamientoLogica;
+
+@Service
+public class DireccionamientoLogicaImpl implements DireccionamientoLogica{
+
+	@Autowired
+	private ConsultaMipres consultaMipres;
+	
+	@Autowired
+	private DireccionamientoDAO direccionamientoDAO;
+	
+	@Override
+	public void procesaDireccionamientos() {
+		
+		List<DireccionamientoDTO> direccionamientos = consultaMipres.consultaDireccionamiento("", "");
+		
+		direccionamientos.forEach(direccio -> {
+			ModelMapper modelMapper = new ModelMapper();
+			Direccionamiento direccionamiento = modelMapper.map(direccio, Direccionamiento.class);
+			direccionamientoDAO.save(direccionamiento);
+		});
+		
+		
+		
+	}
 
 }
